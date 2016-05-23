87614c759297902ff260570410b4d4f6a75b95b2
Dan <danielandrewvoss@gmail.com>
2016-05-23 14:47:41 -0400

    Add outline

--- src/com/mycompany/Main.java
+++ src/com/mycompany/Main.java
@@ -1,6 +1,7 @@
 package com.mycompany;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
 
@@ -8,18 +9,38 @@ public class Main {
 
         ArrayList<InventoryItem> inventory = new ArrayList<>();
 
+        InventoryItem item1 = new InventoryItem("apple", 10);
+        inventory.add(item1);
+        InventoryItem item2 = new InventoryItem("banana", 5);
+        inventory.add(item2);
+
         while (true) {
 
             System.out.println("Here is your inventory: ");
-
-            for (InventoryItem item : inventory) {
-                System.out.println(item);
-            }
 
             System.out.println("What would you like to do?:");
             System.out.println("1. Create a new item");
             System.out.println("2. Remove an item");
             System.out.println("3. Update item quantity");
+
+            Scanner scanner = new Scanner(System.in);
+            String choice = scanner.nextLine();
+
+            switch (choice) {
+                case "1":
+                    //
+                    break;
+                case "2":
+                    //
+                    break;
+                case "3":
+                    //
+                    break;
+                default:
+                    System.out.println("Invalid option.");
+            }
+
+
 
 
         }
